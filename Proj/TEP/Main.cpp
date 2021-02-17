1f877e78a5767ca1b2a66b3c8e7cceaefd593107
Tobsi <tsh40884137@gmail.com>
2021-02-17 17:33:48 +0100

    Init Test
    just added the VS Studio Project.

---
+++ Proj/TEP/Main.cpp
@@ -0,0 +1,13 @@
+#include <Windows.h>
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+
+
+    std::cout << "\n================================================"
+        << "\nEnter any key (not space or enter) to quit: ";
+    int a;
+    std::cin >> a;
+
+    return 0;
+}
